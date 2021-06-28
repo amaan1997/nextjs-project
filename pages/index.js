1dc55c64e929604a66b1f2c013d3df2c31ca21a0
Amaan Salheen <>
2021-06-28 20:14:10 +0530

    minor change

--- pages/index.js
+++ pages/index.js
@@ -21,8 +21,8 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Documentation--12 &rarr;</h3>
+            <p>Find in-depth informations about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
